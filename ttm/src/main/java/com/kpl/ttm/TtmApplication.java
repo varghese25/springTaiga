c9afa53b57105979b2673ace55ee5f133ae02500
varghese25 <vbpoulose@gmail.com>
2024-10-18 14:39:10 -0400

    Feat: Signup JavaScript Completed And SpringBoot TTM  Application Yml done :Yet to Further Process_ 18-10-2024

---
+++ ttm/src/main/java/com/kpl/ttm/TtmApplication.java
@@ -0,0 +1,13 @@
+package com.kpl.ttm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtmApplication.class, args);
+	}
+
+}
---
+++ ttm/src/test/java/com/kpl/ttm/TtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpl.ttm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
